ca35d3f4b2244659dc73bd6e52c4e139f688e660
izzatmammadov <imamedov388@gmail.com>
2023-12-19 01:05:46 +0400

    update

--- src/components/Section_2/Prices.js
+++ src/components/Section_2/Prices.js
@@ -4,9 +4,9 @@ import price from "../../assets/images/prices.jpg"
 export const Prices = () => {
   return (
     <section className='h-screen relative flex justify-center items-center text-white'>
-        <h3 className='absolute top-10 text-xl font-bold text-center'>Our Pricing</h3>
+        <h3 className='absolute z-10 top-10 text-xl font-bold text-center'>Our Pricing</h3>
         <div className="h-screen"><img className="h-screen w-screen filter grayscale "src={price}/></div>
-    <section className='absolute top-30 flex flex-wrap w-full items-center justify-center gap-6'>
+    <section className='absolute top-36 flex flex-wrap w-full items-center justify-center gap-6'>
 
         <div className='flex items-center justify-between w-[40%] p-8 border-4 border-main cursor-pointer text-m hover:transform hover:scale-105 transition-all duration-500 backdrop-blur-md'>
             <p className='font-semibold'>Minimalist</p>
